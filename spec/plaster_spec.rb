f5c3fb3655ad1e8e4e7010e615244f9e14cf32bb
Steve Jorgensen <stevej@stevej.name>
2015-07-04 06:39:30 -0700

    whitespace and spec example wording

--- spec/plaster_spec.rb
+++ spec/plaster_spec.rb
@@ -8,42 +8,42 @@ module PlasterSpec
     attribute :bar, String
   end
 
-describe Plaster do
-  it 'has a version number' do
-    expect(Plaster::VERSION).not_to be nil
+  describe Plaster do
+    it 'has a version number' do
+      expect(Plaster::VERSION).not_to be nil
+    end
+
+    describe "#deconstruct" do
+      it "returns a simple value for a simple value" do
+        expect( subject.deconstruct( 'abc' ) ).to eq( 'abc' )
+        expect( subject.deconstruct(  123  ) ).to eq(  123  )
+        expect( subject.deconstruct(  1..3 ) ).to eq(  1..3 )
+      end
+
+      it "returns a hash map with indifferent access for a flat hash" do
+        data = subject.deconstruct( {animal: 'goat', legs: 4} )
+        expect( data[ :animal  ] ).to eq('goat')
+        expect( data[ 'animal' ] ).to eq('goat')
+        expect( data[ 'legs'   ] ).to eq( 4    )
+      end
+
+      it "returns an array for a flat array" do
+        data = subject.deconstruct( ['abc', 123] )
+        expect( data ).to eq( ['abc', 123] )
+      end
+
+      it "returns a hash map with indifferent access for a flat struct" do
+        vehicle_class = Struct.new(:type, :wheels)
+        vehicle = vehicle_class.new('tricycle', 3)
+        data = subject.deconstruct( vehicle )
+        expect( data ).to eq( HashWithIndifferentAccess.new(type: 'tricycle', wheels: 3) )
+      end
+
+      it "returns a hash map with indifferent access for a flat Virtus model" do
+        obj = VirtusFooBar.new(foo: 123, bar: 'xyz')
+        expect( subject.deconstruct(obj) ).to eq( HashWithIndifferentAccess.new(foo: 123, bar: 'xyz') )
+      end
+    end
+
   end
-
-  describe "#deconstruct" do
-    it "returns a simple value for a simple value" do
-      expect( subject.deconstruct( 'abc' ) ).to eq( 'abc' )
-      expect( subject.deconstruct(  123  ) ).to eq(  123  )
-      expect( subject.deconstruct(  1..3 ) ).to eq(  1..3 )
-    end
-
-    it "returns a hash with indifferent access for a flat hash" do
-      data = subject.deconstruct( {animal: 'goat', legs: 4} )
-      expect( data[ :animal  ] ).to eq('goat')
-      expect( data[ 'animal' ] ).to eq('goat')
-      expect( data[ 'legs'   ] ).to eq( 4    )
-    end
-
-    it "returns an array for a flat array" do
-      data = subject.deconstruct( ['abc', 123] )
-      expect( data ).to eq( ['abc', 123] )
-    end
-
-    it "returns a hash with indifferent access for a flat struct" do
-      vehicle_class = Struct.new(:type, :wheels)
-      vehicle = vehicle_class.new('tricycle', 3)
-      data = subject.deconstruct( vehicle )
-      expect( data ).to eq( HashWithIndifferentAccess.new(type: 'tricycle', wheels: 3) )
-    end
-
-    it "returns a hash with indifferent access for a flat Virtus model" do
-      obj = VirtusFooBar.new(foo: 123, bar: 'xyz')
-      expect( subject.deconstruct(obj) ).to eq( HashWithIndifferentAccess.new(foo: 123, bar: 'xyz') )
-    end
-
-  end
-end
 end
